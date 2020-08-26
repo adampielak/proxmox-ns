09d5c5a25e01bbb4f4106c27c7763282105cd8aa
Stephen Hepner <5170785+shepner@users.noreply.github.com>
2020-08-26 16:02:03 -0500

    Update dnsmasq.sh

--- docker/dnsmasq.sh
+++ docker/dnsmasq.sh
@@ -1,25 +1,29 @@
 #!/bin/sh
-# Portainer
-# https://www.portainer.io
-# https://www.portainer.io/installation/
+# https://github.com/shepner/Docker-DNSmasq
 
-# Portainer server
+# Combined DNS and DHCP server
 
-sudo docker volume create portainer_data
+# WARNING: This runs with privledged rights
 
-NAME=portainer
+NAME=dnsmasq
 IMAGE=portainer/portainer
+BASEPATH=/mnt/nas/docker/$NAME
+
+sudo mkdir -p $BASEPATH/config
+#sudo wget -O $BASEPATH/config/dnsmasq_dhcp.conf https://raw.githubusercontent.com/shepner/Docker-DNSmasq/master/dnsmasq.conf
+#sudo wget -O $BASEPATH/config/dnsmasq.conf https://raw.githubusercontent.com/shepner/Docker-DNSmasq/master/dnsmasq.conf
+#sudo wget -O $BASEPATH/resolv.conf https://raw.githubusercontent.com/shepner/Docker-DNSmasq/master/resolv.conf
 
 sudo docker pull $IMAGE
 sudo docker stop $NAME
 sudo docker rm -v $NAME
 
 sudo docker run --detach --restart=always \
-  --name $NAME \
+  --name=$NAME \
   --cpus=2 \
-  --cpu-shares=1024 \
-  --publish published=8000,target=8000,protocol=tcp,mode=ingress \
-  --publish published=9000,target=9000,protocol=tcp,mode=ingress \
-  -v /var/run/docker.sock:/var/run/docker.sock \
-  -v portainer_data:/data \
+  --cpu-shares=2048 \
+  --privileged \
+  --env DNSMASQ_CONF=/mnt/config/dnsmasq_combined.conf \
+  --mount type=bind,src=$BASEPATH,dst=/mnt \
+  --network host \
   $IMAGE
